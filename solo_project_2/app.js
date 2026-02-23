91f804a63ebd3570743b84c51c42949206c62516
Shihab Abdelrahim <shihabmouiz@gmail.com>
2026-02-22 20:53:00 -0500

    Fix: update API base URL to production endpoint

--- solo_project_2/app.js
+++ solo_project_2/app.js
@@ -1,6 +1,6 @@
 /*https://soloproj.netlify.app/*/
 
-const API_BASE = "http://127.0.0.1:5000/api";
+const API_BASE = "https://solo-project-2-7rmr.onrender.com/api";
 
 let totalCount = 0;
 let totalPages = 1;
